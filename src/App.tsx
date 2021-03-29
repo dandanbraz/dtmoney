3cd30db08b583453e35df3d0292f7194bcbb055b
DESKTOP-981ATQS\Danilo <dandanbraz98@hotmail.com>
2021-03-29 01:15:07 -0300

    Aplicar o uso do UseContext para utilizar propriedade e funções de outros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
@@ -30,7 +31,7 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,8 +17,6 @@ export function Header ({ onOpenNewTransactionModal }: HeaderProps){
         <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
-        
-        
       </Content>      
     </Container>
   )
